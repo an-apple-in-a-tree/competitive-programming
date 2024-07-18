7a309dd38e567a54618ea2569da617c80e367080
Gloria Li <91165555+an-apple-in-a-tree@users.noreply.github.com>
2024-07-18 12:13:07 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int n, x, num;
+vector<int> coins;
+
+int combine (vector<int> ways, int sum) {
+    for(int c: coins) {
+        if(sum-c >= 0) {
+            ways[sum] += combine(ways, sum-c);
+        }
+    }
+    return ways[sum];
+}
+int main() {
+    cin >> n >> x;
+    vector ways (x+1, 0);
+    ways[0] = 1;
+    for(int i = 0; i < n; i++) {
+        cin >> num;
+        coins.push_back(num);
+    }
+    cout << combine(ways,x) << endl;
+    return 0;
+}
